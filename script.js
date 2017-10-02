6cbafdea8540cfdba5d813459cb867dd6ee9f9e4
DWW1993 <daniel.w.walker1993@gmail.com>
2017-10-02 15:33:00 -0500

    shift key displays correct keyboard

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 var sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+$(document).ready(function() {
+    $("#keyboard-upper-container").hide();
+    $(document).keydown("16", function (){
+        $("#keyboard-upper-container").show();
+        $("#keyboard-lower-container").hide();
+    })
+    $(document).keyup("16", function (){
+        $("#keyboard-upper-container").hide();
+        $("#keyboard-lower-container").show();
+    })
+});
